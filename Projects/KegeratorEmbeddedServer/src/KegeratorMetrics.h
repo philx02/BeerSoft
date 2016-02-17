4d63e3e7cf7775d78b9bf25d29964b762c1ea144
Philippe Cayouette <pcayouette@spoluck.ca>
2016-02-17 10:30:24 -0500

    change in kegerator metrics

--- Projects/KegeratorEmbeddedServer/src/KegeratorMetrics.h
+++ Projects/KegeratorEmbeddedServer/src/KegeratorMetrics.h
@@ -3,19 +3,49 @@
 #include "Subject.h"
 #include <cassert>
 #include <string>
+#include <atomic>
+#include <array>
+#include <fstream>
 
 #include <boost/noncopyable.hpp>
+#include <boost/filesystem.hpp>
+
+namespace bfs = boost::filesystem;
 
 class KegaratorMetrics : public Subject< KegaratorMetrics >
 {
 public:
-  KegaratorMetrics()
+  KegaratorMetrics(const bfs::path &iDataFolder)
   {
+    auto wRetrievePulses = [&](size_t wKegId, const char *iFilename)
+    {
+      mKegsActualPulsesFiles[wKegId].open((iDataFolder / iFilename).c_str());
+      size_t wRetrievedPulses;
+      mKegsActualPulsesFiles[wKegId] >> wRetrievedPulses;
+      mData.mKegsActualPulses[wKegId].store(wRetrievedPulses);
+    };
+    wRetrievePulses(0, "Keg0Pulses.txt");
+    wRetrievePulses(1, "Keg1Pulses.txt");
+    auto wRetrieveValue = [&](const char *iFilename) -> size_t
+    {
+      std::ifstream wFile(iFilename);
+      size_t wValue;
+      wFile >> wValue;
+      return wValue;
+    };
+    mCo2TankEmptyMassIndex = wRetrieveValue("Co2TankEmptyMassIndex.txt");
+    mCo2TankFullMassIndex = wRetrieveValue("Co2TankFullMassIndex.txt");
+    mFullKegTotalPulses = wRetrieveValue("FullKegTotalPulses.txt");
   }
 
   KegaratorMetrics(KegaratorMetrics &&iKegaratorMetrics)
     : mData(std::move(iKegaratorMetrics.mData))
+    , mCo2TankEmptyMassIndex(std::move(iKegaratorMetrics.mCo2TankEmptyMassIndex))
+    , mCo2TankFullMassIndex(std::move(iKegaratorMetrics.mCo2TankFullMassIndex))
+    , mFullKegTotalPulses(std::move(iKegaratorMetrics.mFullKegTotalPulses))
   {
+    mKegsActualPulsesFiles[0] = std::move(iKegaratorMetrics.mKegsActualPulsesFiles[0]);
+    mKegsActualPulsesFiles[1] = std::move(iKegaratorMetrics.mKegsActualPulsesFiles[1]);
   }
 
   void setTemperature(double iTemperature)
@@ -39,7 +69,7 @@ public:
   void pulseKeg(size_t iKegIndex)
   {
     assert(iKegIndex < NUMBER_OF_KEGS);
-    ++mData.mKegsTotalPulses[iKegIndex];
+    ++mData.mKegsActualPulses[iKegIndex];
     Subject< KegaratorMetrics >::notify(*this);
   }
 
@@ -50,12 +80,27 @@ public:
     Data()
       : mTemperature(0)
       , mAmbientPressure(0)
-      , mKegsTotalPulses(NUMBER_OF_KEGS, 0)
     {
+      for (auto &&wKegPulses : mKegsActualPulses)
+      {
+        wKegPulses = 0;
+      }
     }
-    double mTemperature;
-    double mAmbientPressure;
-    std::vector< size_t > mKegsTotalPulses;
+
+    Data(const Data &iData)
+      : mTemperature(iData.mTemperature.load())
+      , mAmbientPressure(iData.mAmbientPressure.load())
+    {
+      for (auto wKegPulsesIndex = 0u; wKegPulsesIndex < mKegsActualPulses.size(); ++wKegPulsesIndex)
+      {
+        mKegsActualPulses[wKegPulsesIndex] = iData.mKegsActualPulses[wKegPulsesIndex].load();
+      }
+    }
+
+    std::atomic< double > mTemperature;
+    std::atomic< double > mAmbientPressure;
+    std::atomic< size_t > mMassIndexOfCo2;
+    std::array< std::atomic< size_t >, NUMBER_OF_KEGS > mKegsActualPulses;
   };
 
   const Data &getData() const
@@ -65,9 +110,20 @@ public:
 
   const std::string dataString() const
   {
-    return std::to_string(mData.mTemperature) + "," + std::to_string(mData.mAmbientPressure) + "," + std::to_string(mData.mKegsTotalPulses[0]) + "," + std::to_string(mData.mKegsTotalPulses[1]);
+    auto wCo2Ratio = mData.mMassIndexOfCo2 < mCo2TankFullMassIndex ? static_cast< double >(mData.mMassIndexOfCo2 - mCo2TankEmptyMassIndex) / static_cast< double >(mCo2TankFullMassIndex - mCo2TankEmptyMassIndex) : 1.0;
+    auto wKeg0Ratio = mData.mKegsActualPulses[0] < mFullKegTotalPulses ? static_cast< double >(mFullKegTotalPulses - mData.mKegsActualPulses[0]) / static_cast< double >(mFullKegTotalPulses) : 0.0;
+    auto wKeg1Ratio = mData.mKegsActualPulses[1] < mFullKegTotalPulses ? static_cast< double >(mFullKegTotalPulses - mData.mKegsActualPulses[1]) / static_cast< double >(mFullKegTotalPulses) : 0.0;
+    return    std::to_string(mData.mTemperature)
+      + "," + std::to_string(mData.mAmbientPressure)
+      + "," + std::to_string(wCo2Ratio)
+      + "," + std::to_string(wKeg0Ratio)
+      + "," + std::to_string(wKeg1Ratio);
   }
 
 private:
   Data mData;
+  size_t mCo2TankEmptyMassIndex;
+  size_t mCo2TankFullMassIndex;
+  size_t mFullKegTotalPulses;
+  std::array< std::fstream, NUMBER_OF_KEGS > mKegsActualPulsesFiles;
 };
