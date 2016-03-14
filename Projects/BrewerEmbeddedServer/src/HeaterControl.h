187f459805471e910625f6dc6f274728240a350d
Philippe Cayouette <Philippe Cayouette@localhost>
2016-03-14 09:41:04 -0400

    work on HeaterControl

--- Projects/BrewerEmbeddedServer/src/HeaterControl.h
+++ Projects/BrewerEmbeddedServer/src/HeaterControl.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <boost/asio.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
 #include <fstream>
 
 class HeaterControl
@@ -9,6 +12,9 @@ public:
     : mPwmGpio(iPwmGpio)
     , mTemperatureCommand(0)
     , mActualTemperature(0)
+    , mIoService(std::make_unique< boost::asio::io_service >())
+    , mDeadlineTimer(makeDeadlineTimer())
+    , mPwmThread(std::make_unique< std::thread >([&]() { mIoService->run(); }))
   {
   }
 
@@ -16,6 +22,9 @@ public:
     : mPwmGpio(std::move(iHeaterControl.mPwmGpio))
     , mTemperatureCommand(iHeaterControl.mTemperatureCommand)
     , mActualTemperature(iHeaterControl.mActualTemperature)
+    , mIoService(std::move(iHeaterControl.mIoService))
+    , mDeadlineTimer(std::move(iHeaterControl.mDeadlineTimer))
+    , mPwmThread(std::move(iHeaterControl.mPwmThread))
   {
   }
 
@@ -40,7 +49,17 @@ public:
   }
 
 private:
+  std::unique_ptr< boost::asio::deadline_timer > makeDeadlineTimer()
+  {
+    auto wDeadlineTimer = std::make_unique< boost::asio::deadline_timer >(*mIoService, boost::posix_time::milliseconds(10));
+    wDeadlineTimer->async_wait([](const boost::system::error_code &) {});
+    return wDeadlineTimer;
+  }
+
   std::ofstream mPwmGpio;
   double mTemperatureCommand;
   double mActualTemperature;
+  std::unique_ptr< boost::asio::io_service > mIoService;
+  std::unique_ptr< boost::asio::deadline_timer > mDeadlineTimer;
+  std::unique_ptr< std::thread > mPwmThread;
 };
