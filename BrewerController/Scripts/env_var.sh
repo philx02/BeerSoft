361a9b396260f5b403cc2237cd9269f829dbd8a2
Philippe Cayouette <pcayouette@spoluck.ca>
2016-04-04 07:55:38 -0400

    scripts

---
+++ BrewerController/Scripts/env_var.sh
@@ -0,0 +1,3 @@
+export PWM_GPIO_DEVICE=/sys/class/gpio/gpio17/value
+export MCP3424_DEVICE_PATH=/sys/bus/i2c/devices/1-0068/iio:device0/in_voltage3_raw
+export RTD_ADC_TEMPERATURE_CALIBRATION=/home/pi/Brewer/calib.csv
---
+++ BrewerController/Scripts/init_gpio.sh
@@ -0,0 +1,4 @@
+echo 17 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio17/direction
+echo 27 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio27/direction
