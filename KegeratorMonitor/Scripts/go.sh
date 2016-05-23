ad78c88b1e8b85b4910dc0cc7c8e58ee167fe84f
Philippe Cayouette <pcayouette@spoluck.ca>
2016-05-23 16:16:06 -0400

    updated scripts and readme

--- KegeratorMonitor/Scripts/go.sh
+++ KegeratorMonitor/Scripts/go.sh
@@ -14,5 +14,4 @@ export LD_LIBRARY_PATH=.
 export I2C_BUS_PATH=/dev/i2c-0
 export FLOW_METER_0_GPIO_PATH=/sys/class/gpio/gpio30/value
 export FLOW_METER_1_GPIO_PATH=/sys/class/gpio/gpio31/value
-./KegaratorMonitor
-
+./KegaratorMonitor data/
