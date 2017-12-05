0b9c9bd035a3bf76f3a953d5b272ee626cad86b1
Philippe Cayouette <pcayouette@spoluck.ca>
2017-12-05 09:47:33 -0500

    now sending now_data upon connected

--- FermentationController/FermCtrl/producerconsumer.py
+++ FermentationController/FermCtrl/producerconsumer.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 from data_collection import *
 
-producer_condition = asyncio.Condition(lock=lock, loop=loop)
+PRODUCER_CONDITION = asyncio.Condition(lock=LOCK, loop=LOOP)
 
 class FermDataSnapshot:
     def __init__(self, timestamp, ferm_data):
@@ -17,14 +17,23 @@ class FermDataSnapshot:
     def serialize(self):
         return str(self.timestamp) + ",%.2f" % self.wort_temperature + ",%.2f" % self.chamber_temperature + ",%.2f" % self.chamber_humidity + ",%.2f" % self.wort_density + "," + ("1" if ferm_data.cooling_status else "0")
 
-historical_data = []
-historical_lock = asyncio.Lock(loop=loop)
+HISTORICAL_DATA = []
+HISTORICAL_LOCK = asyncio.Lock(loop=LOOP)
+
+def generate_message(ferm_data):
+    return "now_data|%.2f" % ferm_data.wort_temperature.get_mean() + "," + "%.2f" % ferm_data.chamber_temperature.get_mean() + "," + "%.2f" % ferm_data.chamber_humidity.get_mean() + "," + "%.2f" % ferm_data.wort_density.get_mean() + "," + ("1" if ferm_data.cooling_status else "0")
+
+@asyncio.coroutine
+def init(websocket):
+    with (yield from LOCK):
+        message = generate_message(FERM_DATA)
+    yield from websocket.send(message)
 
 @asyncio.coroutine
 def produce(websocket):
-    with (yield from producer_condition):
-        yield from producer_condition.wait()
-        message = "now_data|%.2f" % ferm_data.wort_temperature.get_mean() + "," + "%.2f" % ferm_data.chamber_temperature.get_mean() + "," + "%.2f" % ferm_data.chamber_humidity.get_mean() + "," + "%.2f" % ferm_data.wort_density.get_mean() + "," + ("1" if ferm_data.cooling_status else "0")
+    with (yield from PRODUCER_CONDITION):
+        yield from PRODUCER_CONDITION.wait()
+        message = generate_message(FERM_DATA)
     yield from websocket.send(message)
 
 @asyncio.coroutine
@@ -32,29 +41,29 @@ def consume(websocket):
     message = yield from websocket.recv()
     if message == "get_historical_data":
         response = "historical_data|"
-        with (yield from historical_lock):
-            for data_point in historical_data:
+        with (yield from HISTORICAL_LOCK):
+            for data_point in HISTORICAL_DATA:
                 response += data_point.serialize() + ";"
         yield from websocket.send(response)
 
 @asyncio.coroutine
 def notify_clients():
-    with (yield from producer_condition):
-        producer_condition.notify_all()
+    with (yield from PRODUCER_CONDITION):
+        PRODUCER_CONDITION.notify_all()
     yield from asyncio.sleep(1)
     asyncio.ensure_future(notify_clients())
 
 @asyncio.coroutine
 def store_historical_data():
     yield from asyncio.sleep(60)
-    with (yield from lock):
-        with (yield from historical_lock):
-            historical_data.append(FermDataSnapshot(int(datetime.now().timestamp()), ferm_data))
+    with (yield from LOCK):
+        with (yield from HISTORICAL_LOCK):
+            HISTORICAL_DATA.append(FermDataSnapshot(int(datetime.now().timestamp()), FERM_DATA))
     asyncio.ensure_future(store_historical_data())
 
-loop.run_until_complete(loop.create_datagram_endpoint(WortTemperatureProtocol, sock=create_socket(THERMOCOUPLE_PORT)))
-loop.run_until_complete(loop.create_datagram_endpoint(ChamberTemperatureHumidityProtocol, sock=create_socket(CHAMBER_TEMP_HUM_PORT)))
-loop.run_until_complete(loop.create_datagram_endpoint(WortDensityProtocol, sock=create_socket(DENSITY_METER_PORT)))
-loop.run_until_complete(loop.create_datagram_endpoint(CoolingStatusProtocol, sock=create_socket(COOLING_STATUS_PORT)))
+LOOP.run_until_complete(LOOP.create_datagram_endpoint(WortTemperatureProtocol, sock=create_socket(THERMOCOUPLE_PORT)))
+LOOP.run_until_complete(LOOP.create_datagram_endpoint(ChamberTemperatureHumidityProtocol, sock=create_socket(CHAMBER_TEMP_HUM_PORT)))
+LOOP.run_until_complete(LOOP.create_datagram_endpoint(WortDensityProtocol, sock=create_socket(DENSITY_METER_PORT)))
+LOOP.run_until_complete(LOOP.create_datagram_endpoint(CoolingStatusProtocol, sock=create_socket(COOLING_STATUS_PORT)))
 asyncio.ensure_future(notify_clients())
 asyncio.ensure_future(store_historical_data())
