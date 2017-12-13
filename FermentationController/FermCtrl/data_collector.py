03b36e849f607fae3bef3ff3e97f0f27007ae19b
Philippe Cayouette <pcayouette@spoluck.ca>
2017-12-12 20:03:23 -0500

    classified

--- FermentationController/FermCtrl/data_collector.py
+++ FermentationController/FermCtrl/data_collector.py
@@ -6,36 +6,49 @@ import argparse
 from datetime import datetime
 
 from data_collection import *
-
-parser = argparse.ArgumentParser(description='Data Collector for Fermentation Controller.')
-parser.add_argument('-p', '--period', help='Period in seconds', required=True)
-
-args = parser.parse_args()
-
-PERIOD = float(args.period)
+from create_socket import create_socket
 
 @asyncio.coroutine
-def print_data():
-    global ferm_data
+def print_data(ferm_data, lock, period):
     with (yield from lock):
         print(str(int(datetime.now().timestamp())) + "," + "%.2f" % ferm_data.wort_temperature.get_mean() + "," + "%.2f" % ferm_data.chamber_temperature.get_mean() + "," + "%.2f" % ferm_data.chamber_humidity.get_mean() + "," + "%.2f" % ferm_data.wort_density.get_mean() + "," + ("1" if ferm_data.cooling_status else "0"))
     sys.stdout.flush()
-    yield from asyncio.sleep(PERIOD)
-    asyncio.ensure_future(print_data())
+    yield from asyncio.sleep(period)
+    asyncio.ensure_future(print_data(ferm_data, lock, period))
 
-wort_temperature_transport = loop.run_until_complete(loop.create_datagram_endpoint(WortTemperatureProtocol, sock=create_socket(THERMOCOUPLE_PORT)))
-chamber_temperature_humidity_transport = loop.run_until_complete(loop.create_datagram_endpoint(ChamberTemperatureHumidityProtocol, sock=create_socket(CHAMBER_TEMP_HUM_PORT)))
-wort_densit_transporty = loop.run_until_complete(loop.create_datagram_endpoint(WortDensityProtocol, sock=create_socket(DENSITY_METER_PORT)))
-cooling_status_transport = loop.run_until_complete(loop.create_datagram_endpoint(CoolingStatusProtocol, sock=create_socket(COOLING_STATUS_PORT)))
-loop.run_until_complete(print_data())
+def main():
+    parser = argparse.ArgumentParser(description='Data Collector for Fermentation Controller.')
+    parser.add_argument('-p', '--period', help='Period in seconds', required=True)
 
-try:
-    loop.run_forever()
-except KeyboardInterrupt:
-    pass
+    args = parser.parse_args()
 
-wort_temperature_transport.close()
-chamber_temperature_humidity_transport.close()
-wort_densit_transporty.close()
-cooling_status_transport.close()
-loop.close()
+    period = float(args.period)
+
+    ferm_data = FermData()
+
+    loop = asyncio.get_event_loop()
+
+    lock = asyncio.Lock(loop=loop)
+
+    wt_transport, wt_protocol = loop.run_until_complete(loop.create_datagram_endpoint(WortTemperatureProtocol, sock=create_socket(MCAST_GRP, THERMOCOUPLE_PORT)))
+    wt_protocol.setup(ferm_data, lock)
+    cth_transport, cth_protocol = loop.run_until_complete(loop.create_datagram_endpoint(ChamberTemperatureHumidityProtocol, sock=create_socket(MCAST_GRP, CHAMBER_TEMP_HUM_PORT)))
+    cth_protocol.setup(ferm_data, lock)
+    wd_transport, wd_protocol = loop.run_until_complete(loop.create_datagram_endpoint(WortDensityProtocol, sock=create_socket(MCAST_GRP, DENSITY_METER_PORT)))
+    wd_protocol.setup(ferm_data, lock)
+    cs_transport, cs_protocol = loop.run_until_complete(loop.create_datagram_endpoint(CoolingStatusProtocol, sock=create_socket(MCAST_GRP, COOLING_STATUS_PORT)))
+    cs_protocol.setup(ferm_data, lock)
+    loop.run_until_complete(print_data(ferm_data, lock, period))
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+
+    wt_transport.close()
+    cth_transport.close()
+    wd_transport.close()
+    cs_transport.close()
+    loop.close()
+
+main()
--- FermentationController/FermCtrl/websocketserver.py
+++ FermentationController/FermCtrl/websocketserver.py
@@ -1,39 +1,39 @@
 import asyncio
 import websockets
-import producerconsumer
 
-connected = set()
+class WebSocketServer:
+    def __init__(self, on_connect, consumer_handler, producer_handler, port):
+        self.on_connect = on_connect
+        self.consume = consumer_handler
+        self.produce = producer_handler
+        self.connected = set()
+        self.start_server = websockets.serve(self.handler, '0.0.0.0', port)
 
-@asyncio.coroutine
-def consumer_handler(websocket):
-    while True:
-        yield from producerconsumer.consume(websocket)
+    @asyncio.coroutine
+    def consumer_handler(self, websocket):
+        while True:
+            yield from self.consume(websocket)
 
-@asyncio.coroutine
-def producer_handler(websocket):
-    while True:
-        yield from producerconsumer.produce(websocket)
+    @asyncio.coroutine
+    def producer_handler(self, websocket):
+        while True:
+            yield from self.produce(websocket)
 
-@asyncio.coroutine
-def handler(websocket, path):
-    print("client connected")
-    global connected
-    # Register.
-    connected.add(websocket)
-    try:
-        yield from producerconsumer.init(websocket)
-        consumer_task = asyncio.async(consumer_handler(websocket))
-        producer_task = asyncio.async(producer_handler(websocket))
-        done, pending = yield from asyncio.wait(
-            [consumer_task, producer_task],
-            return_when=asyncio.FIRST_COMPLETED,
-        )
-        for task in pending:
-            task.cancel()
-    finally:
-        # Unregister.
-        connected.remove(websocket)
-
-start_server = websockets.serve(handler, '0.0.0.0', 8010)
-
-asyncio.get_event_loop().run_until_complete(start_server)
+    @asyncio.coroutine
+    def handler(self, websocket, path):
+        print("client connected")
+        # Register.
+        self.connected.add(websocket)
+        try:
+            yield from self.on_connect(websocket)
+            consumer_task = asyncio.async(self.consumer_handler(websocket))
+            producer_task = asyncio.async(self.producer_handler(websocket))
+            done, pending = yield from asyncio.wait(
+                [consumer_task, producer_task],
+                return_when=asyncio.FIRST_COMPLETED,
+            )
+            for task in pending:
+                task.cancel()
+        finally:
+            # Unregister.
+            self.connected.remove(websocket)
