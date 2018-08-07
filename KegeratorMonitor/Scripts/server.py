69b6fbd48a023829448ebff548309a0ad2077eeb
Philippe Cayouette <pcayouette@spoluck.ca>
2018-08-07 14:12:13 -0400

    mots

--- KegeratorMonitor/Scripts/server.py
+++ KegeratorMonitor/Scripts/server.py
@@ -9,19 +9,19 @@ class KegeratorServer:
     def __init__(self):
         self.lock = asyncio.Lock()
         self.producer_condition = asyncio.Condition(lock=self.lock)
-        self.kegerator_data = KegeratorData()
+        self.data = KegeratorData()
 
     @asyncio.coroutine
     def init(self, websocket):
         with (yield from self.lock):
-            message = self.kegerator_data.serialize()
+            message = self.data.serialize()
         yield from websocket.send(message)
 
     @asyncio.coroutine
     def produce(self, websocket):
         with (yield from self.producer_condition):
             yield from self.producer_condition.wait()
-            message = self.kegerator_data.serialize()
+            message = self.data.serialize()
         yield from websocket.send(message)
 
     @asyncio.coroutine
@@ -29,7 +29,7 @@ class KegeratorServer:
         message = yield from websocket.recv()
         if message == "get_status":
             with (yield from self.lock):
-                message = self.kegerator_data.serialize()
+                message = self.data.serialize()
             yield from websocket.send(message)
 
     @asyncio.coroutine
@@ -44,18 +44,18 @@ def main():
     kegerator_server = KegeratorServer()
 
     t_transport, t_protocol = loop.run_until_complete(loop.create_datagram_endpoint(TemperatureProtocol, sock=create_socket(MCAST_GRP, THERMOMETER_PORT)))
-    t_protocol.setup(ferm_server.ferm_data, ferm_server.lock)
+    t_protocol.setup(kegerator_server.data, kegerator_server.lock)
     lc_transport, lc_protocol = loop.run_until_complete(loop.create_datagram_endpoint(LoadCellProtocol, sock=create_socket(MCAST_GRP, LOAD_CELL_PORT)))
-    lc_protocol.setup(ferm_server.ferm_data, ferm_server.lock)
+    lc_protocol.setup(kegerator_server.data, kegerator_server.lock)
     k1_transport, k1_protocol = loop.run_until_complete(loop.create_datagram_endpoint(Keg1LevelProtocol, sock=create_socket(MCAST_GRP, KEG1_LEVEL_PORT)))
-    k1_protocol.setup(ferm_server.ferm_data, ferm_server.lock)
+    k1_protocol.setup(kegerator_server.data, kegerator_server.lock)
     k2_transport, k2_protocol = loop.run_until_complete(loop.create_datagram_endpoint(Keg2LevelProtocol, sock=create_socket(MCAST_GRP, KEG2_LEVEL_PORT)))
-    k2_protocol.setup(ferm_server.ferm_data, ferm_server.lock)
+    k2_protocol.setup(kegerator_server.data, kegerator_server.lock)
 
-    websocket_server = WebSocketServer(ferm_server.init, ferm_server.consume, ferm_server.produce, 8011)
+    websocket_server = WebSocketServer(kegerator_server.init, kegerator_server.consume, kegerator_server.produce, 8011)
     loop.run_until_complete(websocket_server.start_server)
 
-    asyncio.ensure_future(ferm_server.notify_clients())
+    asyncio.ensure_future(kegerator_server.notify_clients())
 
     asyncio.get_event_loop().run_forever()
 
