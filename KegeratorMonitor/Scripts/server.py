2929d388158cb7117bb22f21cf8d9eb09bb97586
Philippe Cayouette <pcayouette@spoluck.ca>
2018-08-11 09:04:57 -0400

    ok...

--- KegeratorMonitor/Scripts/server.py
+++ KegeratorMonitor/Scripts/server.py
@@ -2,7 +2,7 @@
 
 import asyncio
 from websocketserver import WebSocketServer
-from create_socket import create_socket_win
+from create_socket import create_socket
 from data_collection import *
 
 class KegeratorServer:
@@ -47,16 +47,16 @@ def main():
     loop = asyncio.get_event_loop()
     kegerator_server = KegeratorServer()
 
-    t_transport, t_protocol = loop.run_until_complete(loop.create_datagram_endpoint(TemperatureProtocol, sock=create_socket_win(MCAST_GRP, THERMOMETER_PORT)))
+    t_transport, t_protocol = loop.run_until_complete(loop.create_datagram_endpoint(TemperatureProtocol, sock=create_socket(MCAST_GRP, THERMOMETER_PORT)))
     t_protocol.setup(kegerator_server.data, kegerator_server.lock)
-    lc_transport, lc_protocol = loop.run_until_complete(loop.create_datagram_endpoint(LoadCellProtocol, sock=create_socket_win(MCAST_GRP, LOAD_CELL_PORT)))
+    lc_transport, lc_protocol = loop.run_until_complete(loop.create_datagram_endpoint(LoadCellProtocol, sock=create_socket(MCAST_GRP, LOAD_CELL_PORT)))
     lc_protocol.setup(kegerator_server.data, kegerator_server.lock)
-    k1_transport, k1_protocol = loop.run_until_complete(loop.create_datagram_endpoint(Keg1LevelProtocol, sock=create_socket_win(MCAST_GRP, KEG0_LEVEL_PORT)))
+    k1_transport, k1_protocol = loop.run_until_complete(loop.create_datagram_endpoint(Keg1LevelProtocol, sock=create_socket(MCAST_GRP, KEG0_LEVEL_PORT)))
     k1_protocol.setup(kegerator_server.data, kegerator_server.lock)
-    k2_transport, k2_protocol = loop.run_until_complete(loop.create_datagram_endpoint(Keg2LevelProtocol, sock=create_socket_win(MCAST_GRP, KEG1_LEVEL_PORT)))
+    k2_transport, k2_protocol = loop.run_until_complete(loop.create_datagram_endpoint(Keg2LevelProtocol, sock=create_socket(MCAST_GRP, KEG1_LEVEL_PORT)))
     k2_protocol.setup(kegerator_server.data, kegerator_server.lock)
 
-    websocket_server = WebSocketServer(kegerator_server.init, kegerator_server.consume, kegerator_server.produce, 8011)
+    websocket_server = WebSocketServer(kegerator_server.init, kegerator_server.consume, kegerator_server.produce, 8012)
     loop.run_until_complete(websocket_server.start_server)
 
     asyncio.ensure_future(kegerator_server.notify_clients())
