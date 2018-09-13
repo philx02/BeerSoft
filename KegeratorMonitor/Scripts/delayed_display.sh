ba402e497b18bed1567d5f2171bb1e4bb14af7bb
Philippe Cayouette <Philippe Cayouette@localhost>
2018-09-13 14:54:32 +0000

    more cleanup

--- KegeratorMonitor/Scripts/delayed_display.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export DISPLAY=:0
-while ! xset q &>/dev/null; do
-  sleep 1
-done
-echo starting...
-/home/pi/KegeratorMonitor/display.sh
---
+++ KegeratorMonitor/Scripts/pulse_counter_0.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+/home/pi/KegeratorMonitor/pulse_counter.py --gpio 17 --udp_port 10002
+
