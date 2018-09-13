573d597f126a11ca141397d62c7599503563c772
Philippe Cayouette <pcayouette@spoluck.ca>
2018-09-13 15:10:25 +0000

    fix

--- KegeratorMonitor/Scripts/MonitorOnOffControl.py
+++ KegeratorMonitor/Scripts/MonitorOnOffControl.py
@@ -19,7 +19,7 @@ sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 hysteresis = 10
 
 while True:
-  frame = sock.recv(10240)
+  frame = sock.recv(10240).decode()
   if len(frame) > 0:
     if frame[0] == '1':
       hysteresis += 1
