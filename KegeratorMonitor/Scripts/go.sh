e7d3738de3250ec7ab2f798203d9708884f777c3
Philippe Cayouette <pcayouette@spoluck.ca>
2020-06-13 18:11:48 +0000

    yeah

--- KegeratorMonitor/Scripts/go.sh
+++ KegeratorMonitor/Scripts/go.sh
@@ -3,6 +3,6 @@ screen -d -S load_cell -m /home/pi/KegeratorMonitor/auto_restart.sh /home/pi/Keg
 screen -d -S temp_sensor -m /home/pi/KegeratorMonitor/auto_restart.sh /home/pi/KegeratorMonitor/temp_sensor.py
 screen -d -S pulse_counter_0 -m /home/pi/KegeratorMonitor/auto_restart.sh /home/pi/KegeratorMonitor/pulse_counter_0.sh
 screen -d -S pulse_counter_1 -m /home/pi/KegeratorMonitor/auto_restart.sh /home/pi/KegeratorMonitor/pulse_counter_1.sh
-screen -d -S server -m /home/pi/KegeratorMonitor/server.py
+screen -d -S server -m /home/pi/KegeratorMonitor/server_py.sh
 screen -d -S display -m /home/pi/KegeratorMonitor/display-kiosk.sh
 screen -d -S on_off -m /home/pi/KegeratorMonitor/MonitorOnOffControl.py
